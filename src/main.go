d4e09de5f430438c8638c5d337e8c3ff479a635e
Wesley Dias <wesley_diaz8@outlook.com>
2025-01-10 21:00:47 -0300

    corrigir porta

--- src/main.go
+++ src/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
@@ -13,6 +14,10 @@ func index(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000" // Porta padrão caso PORT não esteja definida
+	}
 	http.HandleFunc("/", index)
 	log.Fatal(http.ListenAndServe("0.0.0.0:5000", nil))
 }
